030dde0307dbddb59ad5d48f6860c6a72540c72b
Nikivan <59735482+nikivan43@users.noreply.github.com>
2021-10-04 07:39:20 +0200

    Added command loading

--- bot.js
+++ bot.js
@@ -1,6 +1,17 @@
 const { Client, Intents } = require('discord.js');
 const { dtoken } = require('./config.json');
 const fs = require('fs')
+client.commands = new Collection()
+
+const commandFolders = fs.readdirSync('./commands');
+
+for (const folder of commandFolders) {
+	const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const command = require(`./commands/${folder}/${file}`);
+		client.commands.set(command.name, command);
+	}
+}
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
